fe1423c34a7ff9dfcf9bac5c16bb0f645096a4ab
Alexander Chori <alexandrchori@gmail.com>
2020-09-20 20:09:22 +0300

    Complete home assignment

--- Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
+++ Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour
 {
     public MetaBallField Field = new MetaBallField();
-    
+
     private MeshFilter _filter;
     private Mesh _mesh;
-    
+
     private List<Vector3> vertices = new List<Vector3>();
     private List<Vector3> normals = new List<Vector3>();
     private List<int> indices = new List<int>();
@@ -20,10 +20,10 @@ public class MeshGenerator : MonoBehaviour
     {
         // Getting a component, responsible for storing the mesh
         _filter = GetComponent<MeshFilter>();
-        
+
         // instantiating the mesh
         _mesh = _filter.mesh = new Mesh();
-        
+
         // Just a little optimization, telling unity that the mesh is going to be updated frequently
         _mesh.MarkDynamic();
     }
@@ -34,61 +34,80 @@ public class MeshGenerator : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        List<Vector3> cubeVertices = new List<Vector3>
-        {
-            new Vector3(0, 0, 0), // 0
-            new Vector3(0, 1, 0), // 1
-            new Vector3(1, 1, 0), // 2
-            new Vector3(1, 0, 0), // 3
-            new Vector3(0, 0, 1), // 4
-            new Vector3(0, 1, 1), // 5
-            new Vector3(1, 1, 1), // 6
-            new Vector3(1, 0, 1), // 7
-        };
-
-        int[] sourceTriangles =
-        {
-            0, 1, 2, 2, 3, 0, // front
-            3, 2, 6, 6, 7, 3, // right
-            7, 6, 5, 5, 4, 7, // back
-            0, 4, 5, 5, 1, 0, // left
-            0, 3, 7, 7, 4, 0, // bottom
-            1, 5, 6, 6, 2, 1, // top
-        };
-
-        
         vertices.Clear();
         indices.Clear();
         normals.Clear();
-        
+
         Field.Update();
         // ----------------------------------------------------------------
         // Generate mesh here. Below is a sample code of a cube generation.
         // ----------------------------------------------------------------
 
-        // What is going to happen if we don't split the vertices? Check it out by yourself by passing
-        // sourceVertices and sourceTriangles to the mesh.
-        for (int i = 0; i < sourceTriangles.Length; i++)
+        const float step = 0.25f;
+        for (float x = -4; x <= 4; x += step)
         {
-            indices.Add(vertices.Count);
-            Vector3 vertexPos = cubeVertices[sourceTriangles[i]];
-            
-            //Uncomment for some animation:
-            //vertexPos += new Vector3
-            //(
-            //    Mathf.Sin(Time.time + vertexPos.z),
-            //    Mathf.Sin(Time.time + vertexPos.y),
-            //    Mathf.Sin(Time.time + vertexPos.x)
-            //);
-            
-            vertices.Add(vertexPos);
+            for (float y = -4; y <= 4; y += step)
+            {
+                for (float z = -4; z <= 4; z += step)
+                {
+                    var currentPivotVertex = new Vector3(x, y, z);
+
+                    var mask = 0;
+                    const float surfaceLevel = 0;
+                    
+                    for (var i = 0; i < MarchingCubes.Tables._cubeVertices.Length; ++i)
+                    {
+                        var vertexPos = currentPivotVertex + MarchingCubes.Tables._cubeVertices[i] * step;
+
+                        if (Field.F(vertexPos) >= surfaceLevel)
+                        {
+                            mask |= 1 << i;
+                        }
+                    }
+
+                    byte trianglesCount = MarchingCubes.Tables.CaseToTrianglesCount[mask];
+                    for (var i = 0; i < trianglesCount; ++i)
+                    {
+                        var edges = MarchingCubes.Tables.CaseToVertices[mask][i];
+                        for (var j = 0; j < 3; ++j)
+                        {
+                            var A = currentPivotVertex +
+                                    MarchingCubes.Tables._cubeVertices[MarchingCubes.Tables._cubeEdges[edges[j]][0]] *
+                                    step;
+                            var B = currentPivotVertex +
+                                    MarchingCubes.Tables._cubeVertices[MarchingCubes.Tables._cubeEdges[edges[j]][1]] *
+                                    step;
+
+                            var p = (A * Field.F(B) - B * Field.F(A)) / (Field.F(B) - Field.F(A));
+
+                            // Uncomment for some animation:
+                            p += new Vector3
+                            (
+                                Mathf.Sin(Time.time + p.z),
+                                Mathf.Sin(Time.time + p.y),
+                                Mathf.Sin(Time.time + p.x)
+                            );
+
+                            indices.Add(vertices.Count);
+                            vertices.Add(p);
+
+                            const float eps = 0.01f;
+                            normals.Add(new Vector3(
+                                Field.F(p + Vector3.right * eps) - Field.F(p + Vector3.left * eps),
+                                Field.F(p + Vector3.up * eps) - Field.F(p + Vector3.down * eps),
+                                Field.F(p + Vector3.forward * eps) - Field.F(p + Vector3.back * eps)
+                            ).normalized);
+                        }
+                    }
+                }
+            }
         }
 
         // Here unity automatically assumes that vertices are points and hence (x, y, z) will be represented as (x, y, z, 1) in homogenous coordinates
         _mesh.Clear();
         _mesh.SetVertices(vertices);
         _mesh.SetTriangles(indices, 0);
-        _mesh.RecalculateNormals(); // Use _mesh.SetNormals(normals) instead when you calculate them
+        _mesh.SetNormals(normals);
 
         // Upload mesh data to the GPU
         _mesh.UploadMeshData(false);
